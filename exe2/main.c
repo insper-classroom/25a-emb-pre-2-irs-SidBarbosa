91c59902583480811358a11fd87a2cd094ec540e
SidBarbosa <sidneybo@al.insper.edu.br>
2025-02-16 16:15:33 -0300

    exercicios 1 e 2 feitos

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,31 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN = 4;
+const int BTN_PIN = 28;
+volatile int btn_flag = 0;
+
+void btn_callback(uint gpio, uint32_t events) {
+  if(events == 0x4) { // fall edge
+    btn_flag = 1;
+  }
+}
+
 int main() {
   stdio_init_all();
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+  gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
 
   while (true) {
+    if(btn_flag){
+      btn_flag = 0;
+      gpio_put(LED_PIN, !gpio_get(LED_PIN));
+    }
   }
 }
