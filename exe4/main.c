95c86ca571bf38851ba37d18b5b67b02a6a99ca8
SidBarbosa <sidneybo@al.insper.edu.br>
2025-02-16 16:49:56 -0300

    ex4 feito

--- exe4/main.c
+++ exe4/main.c
@@ -2,9 +2,60 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
+
+volatile int btn_r_state = 0;
+volatile int btn_g_state = 0;
+
+void btn_callback(uint gpio, uint32_t events){
+  if (events == 0x4) { // fall edge
+    if (gpio == BTN_PIN_R){
+      btn_r_state = 1;
+    }else if (gpio == BTN_PIN_G){
+      btn_g_state = 1;
+    }
+  }else if(events == 0x8){ // rise edge
+    if(gpio == BTN_PIN_R){
+      btn_r_state = 2;
+    }else if(gpio == BTN_PIN_G){
+      btn_g_state = 2;
+    }
+  }
+}
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_G,GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,true, &btn_callback);
+  
+
   while (true) {
+    if(btn_r_state == 1){
+      btn_r_state = 0;
+      gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+    }
+    if(btn_g_state == 2){
+      btn_g_state = 0;
+      gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
+    }
   }
 }
